3d43d07ce9617ef4efd9ae74c7c873c68546e99c
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2022-12-27 16:25:39 +0300

    [Enh](Transformer): Moved Transformer from demo and added types. Not usable for now

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { KonvaEventObject } from 'konva/lib/Node';
 import React from 'react';
-import { Stage, Layer, Rect, Text } from 'react-konva';
+import Konva from 'konva';
+import { Stage, Layer, Rect, Text, Transformer, KonvaNodeEvents } from 'react-konva';
 import './App.css';
 
 const QUANTITY = 100
@@ -15,15 +16,91 @@ const generateShapes = () => {
   }));
 }
 
+export interface IRectangleProps extends Konva.RectConfig {
+    isSelected: boolean;
+    onSelect: KonvaNodeEvents['onClick'];
+    onChange: (props: { id: string, x: number, y: number, isDragging: boolean, rotation: number }) => void;
+}
+
+const Rectangle = (props: IRectangleProps) => {
+  const { isSelected, onSelect, onChange, rectProps } = props;
+  const shapeRef = React.useRef<Konva.Rect>();
+  const trRef = React.useRef<Konva.Transformer>();
+
+  React.useEffect(() => {
+    if (isSelected && trRef.current && shapeRef.current) {
+      // we need to attach transformer manually
+      trRef.current.nodes([shapeRef.current]);
+      trRef.current.getLayer()?.batchDraw();
+    }
+  }, [isSelected]);
+
+  return (
+    <React.Fragment>
+      <Rect
+        onClick={onSelect}
+        onTap={onSelect}
+        ref={shapeRef as React.LegacyRef<Konva.Rect> | undefined}
+        {...rectProps}
+        draggable
+        onDragEnd={(e) => {
+          onChange({
+            ...rectProps,
+            x: e.target.x(),
+            y: e.target.y(),
+          });
+        }}
+        onTransformEnd={() => {
+          // transformer is changing scale of the node
+          // and NOT its width or height
+          // but in the store we have only width and height
+          // to match the data better we will reset scale on transform end
+          const node = shapeRef.current;
+          if(!node) {
+              return;
+          }
+
+          const scaleX = node.scaleX();
+          const scaleY = node.scaleY();
+
+          // we will reset it back
+          node.scaleX(1);
+          node.scaleY(1);
+          onChange({
+            ...rectProps,
+            x: node.x(),
+            y: node.y(),
+            // set minimal value
+            width: Math.max(5, node.width() * scaleX),
+            height: Math.max(node.height() * scaleY),
+          });
+        }}
+      />
+      {isSelected && (
+        <Transformer
+          ref={trRef as React.LegacyRef<Konva.Transformer> | undefined}
+          boundBoxFunc={(oldBox, newBox) => {
+            // limit resize
+            if (newBox.width < 5 || newBox.height < 5) {
+              return oldBox;
+            }
+            return newBox;
+          }}
+        />
+      )}
+    </React.Fragment>
+  );
+};
+
 const INITIAL_STATE = generateShapes();
 
 export const App = () => {
-  const [timelineRect, setStars] = React.useState(INITIAL_STATE);
+  const [rectangleElements, setRectangleElements] = React.useState(INITIAL_STATE);
 
   const handleDragStart = (e: KonvaEventObject<DragEvent>) => {
     const id = e.target.id();
-    setStars(
-      timelineRect.map((star) => {
+    setRectangleElements(
+      rectangleElements.map((star) => {
         return {
           ...star,
           isDragging: star.id === id,
@@ -32,8 +109,8 @@ export const App = () => {
     );
   };
   const handleDragEnd = (e: KonvaEventObject<DragEvent>) => {
-    setStars(
-      timelineRect.map((star) => {
+    setRectangleElements(
+      rectangleElements.map((star) => {
         return {
           ...star,
           isDragging: false,
@@ -41,13 +118,37 @@ export const App = () => {
       })
     );
   };
+  const [selectedId, selectShape] = React.useState<string | null>(null);
+
+  const checkDeselect = (e: KonvaEventObject<MouseEvent>) => {
+    // deselect when clicked on empty area
+    const clickedOnEmpty = e.target === e.target.getStage();
+    if (clickedOnEmpty) {
+      selectShape(null);
+    }
+  };
 
   return (
-    <Stage width={window.innerWidth} height={window.innerHeight}>
+    <Stage
+        width={window.innerWidth}
+        height={window.innerHeight}
+        onMouseDown={checkDeselect}
+        /* onTouchStart={checkDeselect} */
+      >
       <Layer>
         <Text text="Try to drag a timeline rect" />
-        {timelineRect.map((el) => (
+        {rectangleElements.map((el, i) => (
           <Rect
+            isSelected={el.id === selectedId}
+            onSelect={() => {
+              selectShape(el.id);
+            }}
+            /* onChange={(newAttrs) => { */
+            /*   const rects = rectangleElements.slice(); */
+            /*   rects[i] = newAttrs; */
+            /*   setRectangleElements(rects); */
+            /* }} */
+            opacity={0.8}
             key={el.id}
             x={el.x}
             y={el.y}
